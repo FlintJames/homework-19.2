64042026bb166c4802fc8ec74fb2400b7ad43a03
FlintJames <lyuciana@yandex.ru>
2024-05-17 04:32:42 +0300

    the code has been formatted create catalog_data.json

--- catalog/migrations/0001_initial.py
+++ catalog/migrations/0001_initial.py
@@ -8,37 +8,93 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100, verbose_name='Наименование категории')),
-                ('description', models.TextField(blank=True, null=True, verbose_name='Описание категории')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "name",
+                    models.CharField(
+                        max_length=100, verbose_name="Наименование категории"
+                    ),
+                ),
+                (
+                    "description",
+                    models.TextField(
+                        blank=True, null=True, verbose_name="Описание категории"
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Категория',
-                'verbose_name_plural': 'Категории',
+                "verbose_name": "Категория",
+                "verbose_name_plural": "Категории",
             },
         ),
         migrations.CreateModel(
-            name='Product',
+            name="Product",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100, verbose_name='Наименование')),
-                ('description', models.TextField(blank=True, null=True, verbose_name='Описание')),
-                ('picture', models.ImageField(blank=True, null=True, upload_to='catalog/', verbose_name='Фотография')),
-                ('purchase_price', models.IntegerField(max_length=150, verbose_name='Цена за покупку')),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')),
-                ('updated_at', models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')),
-                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='catalog.category', verbose_name='Категория')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100, verbose_name="Наименование")),
+                (
+                    "description",
+                    models.TextField(blank=True, null=True, verbose_name="Описание"),
+                ),
+                (
+                    "picture",
+                    models.ImageField(
+                        blank=True,
+                        null=True,
+                        upload_to="catalog/",
+                        verbose_name="Фотография",
+                    ),
+                ),
+                (
+                    "purchase_price",
+                    models.IntegerField(max_length=150, verbose_name="Цена за покупку"),
+                ),
+                (
+                    "created_at",
+                    models.DateTimeField(
+                        auto_now_add=True, verbose_name="Дата создания"
+                    ),
+                ),
+                (
+                    "updated_at",
+                    models.DateTimeField(
+                        auto_now=True, verbose_name="Дата последнего изменения"
+                    ),
+                ),
+                (
+                    "category",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="catalog.category",
+                        verbose_name="Категория",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Продукт',
-                'verbose_name_plural': 'Продукты',
+                "verbose_name": "Продукт",
+                "verbose_name_plural": "Продукты",
             },
         ),
     ]
--- catalog/models.py
+++ catalog/models.py
@@ -1,35 +1,39 @@
 from django.db import models
 
 
-NULLABLE = {'blank': True, 'null': True}
+NULLABLE = {"blank": True, "null": True}
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование категории')
-    description = models.TextField(**NULLABLE, verbose_name='Описание категории')
+    name = models.CharField(max_length=100, verbose_name="Наименование категории")
+    description = models.TextField(**NULLABLE, verbose_name="Описание категории")
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(**NULLABLE, verbose_name='Описание')
-    picture = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Фотография')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
-    purchase_price = models.IntegerField(max_length=150, verbose_name='Цена за покупку')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(**NULLABLE, verbose_name="Описание")
+    picture = models.ImageField(
+        upload_to="catalog/", **NULLABLE, verbose_name="Фотография"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+    purchase_price = models.IntegerField(max_length=150, verbose_name="Цена за покупку")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-
-
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
